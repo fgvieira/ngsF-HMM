3c78be98d93a47db42e9a224b5e781170bfd2899
fgvieira <fgarrettvieira@gmail.com>
2014-06-16 15:01:52 +0200

    Minor renaming tweaks

--- read_data.cpp
+++ read_data.cpp
@@ -1,9 +1,10 @@
 #include "shared.hpp"
 
-double*** read_geno(char *in_geno, bool in_bin, bool in_lkl, uint64_t n_ind, uint64_t n_sites){
+// Reads both called genotypes (1 field per site and indiv), genotype lkls or genotype post probs (3 fields per site and indiv)
+double*** read_geno(char *in_geno, bool in_bin, bool in_probs, uint64_t n_ind, uint64_t n_sites){
   uint64_t n_fields;
   // Depending on input we will have either 1 or 3 genot
-  uint64_t n_geno = (in_lkl ? N_GENO : 1);
+  uint64_t n_geno = (in_probs ? N_GENO : 1);
   double *t;
   double *ptr;
   char *buf = init_char(BUFF_LEN, '\0');
@@ -46,17 +47,17 @@ double*** read_geno(char *in_geno, bool in_bin, bool in_lkl, uint64_t n_ind, uin
       // Use last "n_ind * n_geno" columns
       ptr = t + (n_fields - n_ind * n_geno);
       
-      if(in_lkl)
+      if(in_probs)
 	for(uint64_t i = 0; i < n_ind; i++)
-	  for(uint64_t g = 0; g < N_GENO; g++)
-	    geno[i][s][g] = ptr[i*N_GENO+g];
+          for(uint64_t g = 0; g < N_GENO; g++)
+            geno[i][s][g] = ptr[i*N_GENO+g];
       else
 	for(uint64_t i = 0; i < n_ind; i++){
-	  int g = (int) ptr[i];
-	  if(g < 0 || g > 2)
-	    error(__FUNCTION__, "wrong GENO format!");
-	  geno[i][s][g] = log(1);
-	}
+          int g = (int) ptr[i];
+          if(g < 0 || g > 2)
+            error(__FUNCTION__, "wrong GENO format!");
+          geno[i][s][g] = log(1);
+        }
 
       delete [] t;
     }
